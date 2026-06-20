7130922311d1a206567c43cb00364d8d68d9be99
eliotmonaco <eliotmonaco@gmail.com>
2026-06-20 18:11:27 -0500

    use lapply() to gather prep scripts

--- scripts/_run.R
+++ scripts/_run.R
@@ -3,29 +3,24 @@
 # Assign the end of the date range for Essence data download
 end_date <- Sys.Date() - 1
 
+files <- list.files("scripts", pattern = "^\\d-", full.names = TRUE)
+
 source("scripts/fn-prep.R")
-source("scripts/1-setup.R")
-source("scripts/2-get-ess.R")
-source("scripts/3-satscan.R")
-source("scripts/4-aux-data.R")
-source("scripts/5-clust-smry.R")
-source("scripts/6-export-data.R")
+
+lapply(files, source)
 
 
 
 
+# files <- list.files("scripts", pattern = "^\\d-", full.names = TRUE)
+#
 # for (i in 3:1) {
 #   end_date <- Sys.Date() - 1 - i
 #
 #   print(end_date)
 #
 #   source("scripts/fn-prep.R")
-#   source("scripts/1-setup.R")
-#   source("scripts/2-get-ess.R")
-#   source("scripts/3-satscan.R")
-#   source("scripts/4-save.R")
+#
+#   lapply(files, source)
 # }
-
-
-
 
